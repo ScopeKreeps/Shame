4f49b8ffead374b7f88c66d6cd9c0332ec1d2394
Rayno <Rayno@entelect.co.za>
2014-04-02 17:46:42 +0200

    added tests

---
+++ src/Tests/TwitterControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web.Controllers;
+
+namespace Tests
+{
+    [TestClass]
+    public class TwitterControllerTests
+    {
+        [TestMethod]
+        public void ShouldTweetMessage()
+        {
+            var message = "Message " + Guid.NewGuid().ToString();
+            var controller = new TwitterController();
+
+            var result = controller.TweetMessage(message);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldGetPosts()
+        {
+            var controller = new TwitterController();
+
+            var posts = controller.GetPosts();
+
+            Assert.IsTrue(posts.Any());
+        }  
+    }
+}
