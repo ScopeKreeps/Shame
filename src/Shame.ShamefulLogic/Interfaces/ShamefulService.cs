656c3f3d40914c6babb5ec1cb4d16b1f5944d10f
Rayno <Rayno@entelect.co.za>
2014-05-20 07:00:53 +0200

    Moved  shameful logic from the shame.api solution to the shame solution

---
+++ src/Shame.ShamefulLogic/Interfaces/ShamefulService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shame.ShamefulLogic
+{
+    public interface IShamefulService
+    {
+        void NominateFine(string fine);
+
+        string[] GetFines(int count, string screenName);
+    }
+}
